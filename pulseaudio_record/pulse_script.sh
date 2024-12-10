469b60d488d605c76c32877f6743f5e83a486c32
carlos <cvaldez553@gmail.com>
2024-12-10 15:30:47 -0400

    Module initialization FIXED

--- pulseaudio_record/pulse_script.sh
+++ pulseaudio_record/pulse_script.sh
@@ -2,6 +2,6 @@
 
 pactl load-module module-null-sink sink_name=duplex_out sink_properties=device.description=duplex_out
 pactl load-module module-null-sink sink_name=system_out sink_properties=device.description=system_out
-pactl load-module module-loopback source=game_out.monitor
-pactl load-module module-loopback source=game_out.monitor sink=duplex_out
+pactl load-module module-loopback source=system_out.monitor
+pactl load-module module-loopback source=system_out.monitor sink=duplex_out
 pactl load-module module-loopback sink=duplex_out
